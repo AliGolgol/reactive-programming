4c8a93e4f13f72feb10053bb4ba36ac5bc02a1b5
aligolgol <golgol.postbox@gmail.com>
2020-03-17 18:38:59 +0330

    Adding Bootstrap data to initial mongodb. closes #4

--- src/main/java/com/agg/spring/reactive/repository/CategoryRepository.java
+++ src/main/java/com/agg/spring/reactive/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.agg.spring.reactive.repository;
 import com.agg.spring.reactive.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,String> {
+
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/agg/spring/reactive/repository/RecipeRepository.java
+++ src/main/java/com/agg/spring/reactive/repository/RecipeRepository.java
@@ -1,6 +1,7 @@
 package com.agg.spring.reactive.repository;
 
+import com.agg.spring.reactive.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecipeRepository extends CrudRepository<RecipeRepository,String> {
+public interface RecipeRepository extends CrudRepository<Recipe,String> {
 }
