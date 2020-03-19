f184a38bf3375d3ef4ae1bea0773160532c7393c
aligolgol <golgol.postbox@gmail.com>
2020-03-19 18:55:43 +0330

    Creating the IngredientService. Closes #13

---
+++ src/main/java/com/agg/spring/reactive/service/IngredientService.java
@@ -0,0 +1,13 @@
+package com.agg.spring.reactive.service;
+
+import com.agg.spring.reactive.command.IngredientCommand;
+import reactor.core.publisher.Mono;
+
+public interface IngredientService {
+
+    Mono<IngredientCommand> findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
+
+    Mono<IngredientCommand> saveIngredientCommand(IngredientCommand command);
+
+    Mono<Void> deleteById(String recipeId, String idToDelete);
+}
