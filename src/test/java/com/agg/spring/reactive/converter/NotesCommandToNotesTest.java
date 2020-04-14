cdcb4ec2fde7ff1f0e0cbf70d7b5920d715b113c
aligolgol <golgol.postbox@gmail.com>
2020-04-14 21:29:17 +0430

    Creating NotesCommandToNotesTest . Closes #43

---
+++ src/test/java/com/agg/spring/reactive/converter/NotesCommandToNotesTest.java
@@ -0,0 +1,48 @@
+package com.agg.spring.reactive.converter;
+
+import com.agg.spring.reactive.command.NotesCommand;
+import com.agg.spring.reactive.command.NotesCommandToNotes;
+import com.agg.spring.reactive.domain.Notes;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NotesCommandToNotesTest {
+
+    public static final String ID_VALUE = "1";
+    public static final String RECIPE_NOTES = "Notes";
+    NotesCommandToNotes converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new NotesCommandToNotes();
+
+    }
+
+    @Test
+    public void testNullParameter() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObject() throws Exception {
+        assertNotNull(converter.convert(new NotesCommand()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(ID_VALUE);
+        notesCommand.setRecipeNotes(RECIPE_NOTES);
+
+        //when
+        Notes notes = converter.convert(notesCommand);
+
+        //then
+        assertNotNull(notes);
+        assertEquals(ID_VALUE, notes.getId());
+        assertEquals(RECIPE_NOTES, notes.getRecipeNotes());
+    }
+}
